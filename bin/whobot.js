26fe5696ebd7d7954efc869c3f6a2a079c3c201f
Jay Schwane <jrschwane@uwalumni.com>
2017-03-26 09:43:00 -0500

    Revert master to old bot scheme

--- bin/whobot.js
+++ bin/whobot.js
@@ -3,13 +3,15 @@
 /* jshint node: true */
 
 module.exports = function (req, res, next) {
+    
+    // collect info from the POST
     var userName = req.body.user_name,
-        postText = req.body.text,
+        postText = req.body.text;
         
-        // our response
-        botPayload = {
-            text: 'Hello, @' + userName + '! You wrote: "' + postText + '".'
-        };
+    // our response
+    var botPayload = {
+        text: 'Hello, @' + userName + '! You wrote: "' + postText + '".'
+    };
     
     // avoid infinite loop
     if (userName !== 'slackbot') {
--- server.js
+++ server.js
@@ -12,23 +12,42 @@ var express    = require('express'),
     config     = require('./config'),
     mongoose   = require('mongoose'),
 
-    // routes
-    apiRoutes  = require('./routes/routes.js')(app),
-    setupRoute = require('./routes/setup.js')(app),
-    
     // port assignment
-    port       = process.env.PORT || 3000;
+    port       = process.env.PORT || 3000,
     
-    // deprecated?
-    // whobot     = require('./bin/whobot'),
+    // whobot
+    whobot     = require('./bin/whobot');
 
 
 /* ============================== MIDDLEWARE =============================== */
-app.use(morgan('dev'));  // logging
+
+// logging
+app.use(morgan('dev'));
+
+// parse POST request body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// error handler
+app.use(function (err, req, res, next) {
+    console.log(err.stack);
+    res.status(400).send(err.message);
+});
 
 
 /* ============================= CONNECT TO DB ============================= */
 mongoose.connect(config.getDbConnectionString());
+
+
+
+/* ================================ ROUTES ================================= */
+
+// test route
+app.get('/', function (req, res) {
+    res.status(200).send('Hello from WHobot!');
+});
+
+// bot route calls our whobot module
+app.post('/whobot', whobot);
 
 
 /* ============================= START SERVER ============================== */
