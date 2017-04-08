4b35922090f019b7de8ca7ca9b49ffcd03ce7bf6
Peter Martinson <peter.j.martinson@gmail.com>
2017-04-08 14:26:56 -0400

    Add data dictionary
    This should provide a standard against various spellings and
    misspellings of programming skills.  The calling function doesn't exist
    yet, but should normalize all inputs to be lowercase.

---
+++ bin/dataDictionary.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  
+  'JavaScript' : ['javascript', 'js', 'jscript', 'javascr'],
+  'jQuery'     : ['jquery', 'jq', '$'],
+  'AngularJS'  : ['angularjs', 'angular.js', 'angular', 'ajs', 'ng'],
+  'Node.js'    : ['node.js', 'nodejs', 'node'],
+  'ReactJS'    : ['reactjs', 'react.js', 'react', 'rjs'],
+  'Vue.js'     : ['vue.js', 'vuejs', 'vue', 'vjs'],
+  'Python'     : ['python', 'py'],
+  'Ruby'       : ['ruby', 'gem', 'rails']
+
+}
