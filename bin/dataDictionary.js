db800233fe792fea52323ae81ba5752ba5f29f9b
Jay Schwane <jrschwane@uwalumni.com>
2017-04-08 14:20:37 -0500

    Add a few more skills

--- bin/dataDictionary.js
+++ bin/dataDictionary.js
@@ -1,13 +1,19 @@
+/* jshint esversion:6, node: true */
 
 module.exports = {
   
   'JavaScript' : ['javascript', 'js', 'jscript', 'javascr'],
+  'TypeScript' : ['typescript', 'ts'],
+  'es2015'     : ['es6', 'es2015'],
   'jQuery'     : ['jquery', 'jq', '$'],
-  'AngularJS'  : ['angularjs', 'angular.js', 'angular', 'ajs', 'ng'],
+  'AngularJS'  : ['angularjs', 'angular.js', 'angular', 'ajs', 'ng', 'angular1', 'angular2'],
   'Node.js'    : ['node.js', 'nodejs', 'node'],
   'ReactJS'    : ['reactjs', 'react.js', 'react', 'rjs'],
   'Vue.js'     : ['vue.js', 'vuejs', 'vue', 'vjs'],
   'Python'     : ['python', 'py'],
-  'Ruby'       : ['ruby', 'gem', 'rails']
+  'Ruby'       : ['ruby', 'gem', 'rails'],
+  'SQL'        : ['sql', 'mysql'],
+  'MongoDB'    : ['mongo', 'mongodb', 'mongo.db'],
+  'ExpressJS'  : ['express', 'expressjs']
 
-}
+};
