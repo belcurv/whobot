3432e51ad90d5a7d4215c6ae39d6070a8defa211
Jay Schwane <jrschwane@uwalumni.com>
2017-04-11 16:50:19 -0500

    Add a bunch more skills to dictionary

--- bin/dataDictionary.js
+++ bin/dataDictionary.js
@@ -1,19 +1,26 @@
 /* jshint esversion:6, node: true */
 
 module.exports = {
-  
-  'JavaScript' : ['javascript', 'js', 'jscript', 'javascr'],
-  'TypeScript' : ['typescript', 'ts'],
-  'es2015'     : ['es6', 'es2015'],
-  'jQuery'     : ['jquery', 'jq', '$'],
-  'AngularJS'  : ['angularjs', 'angular.js', 'angular', 'ajs', 'ng', 'angular1', 'angular2'],
-  'Node.js'    : ['node.js', 'nodejs', 'node'],
-  'ReactJS'    : ['reactjs', 'react.js', 'react', 'rjs'],
-  'Vue.js'     : ['vue.js', 'vuejs', 'vue', 'vjs'],
-  'Python'     : ['python', 'py'],
-  'Ruby'       : ['ruby', 'gem', 'rails'],
-  'SQL'        : ['sql', 'mysql'],
-  'MongoDB'    : ['mongo', 'mongodb', 'mongo.db'],
-  'ExpressJS'  : ['express', 'expressjs']
+
+    'AngularJS': ['angularjs', 'angular.js', 'angular', 'ajs', 'ng', 'angular1', 'angular2'],
+    'es2015': ['es6', 'es2015'],
+    'C++': ['c++'],
+    'CSS': ['css', 'css3'],
+    'D3.js': ['d3', 'D3', 'd3.js', 'D3.js'],
+    'ExpressJS': ['express', 'expressjs'],
+    'HTML': ['html', 'html5'],
+    'JavaScript': ['javascript', 'js', 'jscript', 'javascr'],
+    'jQuery': ['jquery', 'jq', '$'],
+    'MATLAB': ['matlab', 'Matlab'],
+    'MongoDB': ['mongo', 'mongodb', 'mongo.db'],
+    'Mustache.js': ['{{mustache}}', 'mustache', 'mustache.js'],
+    'Node.js': ['node.js', 'nodejs', 'node'],
+    'Python': ['python', 'py'],
+    'ReactJS': ['reactjs', 'react.js', 'react', 'rjs'],
+    'Ruby': ['ruby', 'gem', 'rails'],
+    'SQL': ['sql', 'mysql'],
+    'SVG': ['svg', 'scaled vector graphics'],
+    'TypeScript': ['typescript', 'ts'],
+    'Vue.js': ['vue.js', 'vuejs', 'vue', 'vjs']
 
 };
