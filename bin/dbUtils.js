b328016758e82d1e08af507d00a9c7385607aea0
Peter Martinson <peter.j.martinson@gmail.com>
2017-04-13 20:09:47 -0400

    Add getAllSkills() - first draft
    getAllSkills() should gather all skills from the database as an object
    populated by arrays of skills.  That object is then beaten into a sorted
    and de-duplicated array, which gets returned to the caller.

---
+++ bin/dbUtils.js
@@ -0,0 +1,24 @@
+/* jshint esversion:6, node:true */
+
+const Profiles   = require('../models/profileModel'),
+      fetchSkill = require('./fetchSkill');
+
+
+function getAllSkills() {
+    Profiles
+        .find({}, 'skills')
+        .exec()
+        .then( (skills_obj) => {
+            var skill_list = [];
+            for (var skill_array in skills_obj) {
+                skills_obj[skill_array].forEach( function(e) {
+                    if ( skill_list.indexOf(e) < 0 ) {
+                        skill_list.push(e);
+                    }
+                });
+                return skill_list.sort();
+        })
+        .catch( (err) => console.log('Error:', err));
+}
+
+console.log(getAllSkills());
