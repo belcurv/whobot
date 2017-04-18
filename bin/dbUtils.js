b90444375d79429ebf5f0e2ece7d641029714e5f
peter.j.martinson <peter.j.martinson@gmail.com>
2017-04-18 12:39:45 -0400

    Add a histogram of skills
    Additions to `dbUtils.js` create a formatted monospace histogram of
    skill frequency in the database.  Currently, it aggregates all skills,
    regardless of team.
    
    To Do:
    1. Change `getAllSkills` to take a team parameter
    2. Create formatted Slack messages for each function
    3. Make the histogram report percentages, not absolute numbers

--- bin/dbUtils.js
+++ bin/dbUtils.js
@@ -46,8 +46,57 @@ module.exports = {
             });
             callback(skill_tally);
         });            
-    }
+    },
 
+    paddedArray : function(skill_obj) {
+        var max_length = 0,
+            string_array = [],
+            pad, skill, pad_length, i, j;
+
+        // populate skills array, find longest skill name
+        for ( skill in skills ) {
+            if ( max_length < skill.length ) {
+                max_length = skill.length;
+            }
+            string_array.push(skill);
+        }
+
+        // pad each skill in array to max_length characters
+        for ( i = 0; i < string_array.length; i++ ) {
+            pad = '';
+            pad_length = max_length - string_array[i].length;
+            for ( j = 0; j < pad_length; j++ ) {
+                pad += ' ';
+            }
+            string_array[i] += pad + ' ';
+        }
+
+        return string_array;   
+    },
+
+    chartSkills : function(callback) {
+        var skill_index = chart_index = 0,
+            chart = [],
+            stat, j,
+            padded_skills;
+
+        countSkills( function(skill_obj) {
+            padded_skills = padded_array(skill_obj);
+            for ( stat in skill_obj ) {
+                if ( skill_obj[stat] ) {
+                    chart.push(padded_skills[skill_index]);
+                    chart[chart_index] += ': ';
+                    for ( j = 0; j < skill_obj[stat]; j++ ) {
+                        chart[chart_index] += '#';
+                    }
+                    // chart[chart_index] += ' (' + skill_obj[stat] + ')'
+                    chart_index++;
+                }
+                skill_index++;
+            }
+            callback(chart);
+        });
+    }
 };
 
 
