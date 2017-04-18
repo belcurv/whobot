511f91ba7120e4cba5753ffa106ff4eb73850d78
Jay Schwane <jrschwane@uwalumni.com>
2017-04-18 01:26:02 -0500

    Add getAllUsers() function to dbUtils and worker logging

--- bin/dbUtils.js
+++ bin/dbUtils.js
@@ -1,31 +1,50 @@
 /* jshint esversion:6, node:true */
 
-const Profiles   = require('../models/profileModel'),
-      fetchSkill = require('./fetchSkill');
+const Profiles = require('../models/profileModel'),
+    fetchSkill = require('./fetchSkill');
 
 
 module.exports = {
-    getAllSkills : function(callback) {
+
+    getAllUsers: function (callback) {
+        Profiles
+            .find({})
+            .exec()
+            .then( (users) => {
+                
+                var list = users.map( (u) => {
+                    return {
+                        name: u.user_name,
+                        team: u.team_domain,
+                        count: (u.skills).length
+                    };
+                });
+                callback(list);
+            });
+
+    },
+
+    getAllSkills: function (callback) {
         Profiles
             .find({}, 'skills')
             .exec()
-            .then( (db_skills) => {
+            .then((db_skills) => {
                 var skill_list = [];
-                db_skills.forEach( (user_skills) => {
-                  user_skills.skills.forEach( (skill) => {
-                    skill_list.push(skill);
-                  });
+                db_skills.forEach((user_skills) => {
+                    user_skills.skills.forEach((skill) => {
+                        skill_list.push(skill);
+                    });
                 });
                 callback(skill_list.sort());
             })
-            .catch( (err) => console.log('Error:', err));
+            .catch((err) => console.log('Error:', err));
     },
 
-    getSkillList : function(callback) {
+    getSkillList: function (callback) {
         var unique_skill_list = [];
-        this.getAllSkills( (full_skill_list) => {
-            full_skill_list.forEach( (skill) => {
-                if ( unique_skill_list.indexOf(skill) < 0 ) {
+        this.getAllSkills((full_skill_list) => {
+            full_skill_list.forEach((skill) => {
+                if (unique_skill_list.indexOf(skill) < 0) {
                     unique_skill_list.push(skill);
                 }
             });
@@ -33,26 +52,23 @@ module.exports = {
         });
     },
 
-    countSkills : function(callback) {
+    countSkills: function (callback) {
         var skill_tally = {};
-        this.getAllSkills( (full_skill_list) => {
-            full_skill_list.forEach( (skill) => {
-                if ( skill_tally[skill] === undefined ) {
+        this.getAllSkills((full_skill_list) => {
+            full_skill_list.forEach((skill) => {
+                if (skill_tally[skill] === undefined) {
                     skill_tally[skill] = 1;
-                }
-                else {
+                } else {
                     skill_tally[skill] += 1;
                 }
             });
             callback(skill_tally);
-        });            
+        });
     }
 
 };
 
 
 function repopulateSkills() {
-  // push all skills in DB through the data dictionary
+    // push all skills in DB through the data dictionary
 }
-
-
--- bin/worker.js
+++ bin/worker.js
@@ -11,11 +11,11 @@
 */
 
 const schedule = require('node-schedule'),
-      dbUtils  = require('./dbUtils'),
-      
-      // db
-      db         = require('../db'),
-      mongoose   = require('mongoose');
+    dbUtils = require('./dbUtils'),
+
+    // db
+    db = require('../db'),
+    mongoose = require('mongoose');
 
 /* ============================= CONNECT TO DB ============================= */
 
@@ -23,12 +23,26 @@ mongoose.connect(db.getDbConnectionString());
 mongoose.Promise = global.Promise;
 
 
+/* ============================ UTILITY METHODS ============================ */
+
+function buildTeamsList(data) {
+    
+    var seen = {};
+
+    // de-dupelicate teams
+    return data
+        .map( (u) => u.team )
+        .filter( (i) => seen.hasOwnProperty(i) ? false : (seen[i] = true) );
+
+}
+
+
 /* ============================== WORKER MAYBE ============================= */
 
 var worker = {
-    
-    scheduleJob: function() {
-        
+
+    scheduleJob: function () {
+
         /* rules
          *
          * See http://stackoverflow.com/a/5398044/1252653
@@ -36,37 +50,54 @@ var worker = {
          *   every minute:  * * * * *
          *   every midnight: 0 0 * * *
          *
-        */
+         */
         var rule = '* * * * *';
-        
+
         schedule.scheduleJob(rule, function () {
-            console.log(`=========================================================\nSkill stats as of ${new Date()} \n=========================================================`);
-            
-            dbUtils.countSkills(function (results) {
-                
-                var keys = Object.keys(results),
-                    
-                    arr  = keys.map( (key) => [key, results[key]] )
-                               .sort( (a, b) => b[1] - a[1] );
-                
-                console.log(arr);
-                
+
+            console.log([
+                '=========================================================',
+                `Skill stats as of ${new Date()}`,
+                '========================================================='
+            ].join('\n'));
+
+
+            dbUtils.getAllUsers(function (users) {
+
+                var numUsers = users.length,
+                    teams    = buildTeamsList(users);
+
+                console.log(`Total teams: ${teams.length}`);
+                console.log(`Teams: ${teams.join(', ')}`);
+
             });
-            
+
+
+            dbUtils.countSkills(function (results) {
+
+                var keys = Object.keys(results),
+
+                    arr = keys.map((key) => [key, results[key]])
+                    .sort((a, b) => b[1] - a[1]);
+
+                console.log(`\nSkills:\n - ${arr.join('\n - ')}`);
+
+            });
+
         });
-        
+
     },
-    
-    init: function() {
-        
+
+    init: function () {
+
         this.scheduleJob();
-        
+
     }
-    
+
 };
 
 (function () {
-    
+
     worker.init();
-    
+
 }());
